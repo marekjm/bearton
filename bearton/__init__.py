68c0dd7db51ce47fad0005d3c002ca6545b1c83b
Marek Marecki <marekjm@ozro.pw>
2014-04-12 15:15:53 +0200

    Rendamed conf to config

--- bearton/__init__.py
+++ bearton/__init__.py
@@ -1,9 +1,10 @@
-from . import exceptions
 from . import util
-from . import conf
+from . import config
+from . import page
 from . import schemes
 from . import builder
 from . import servers
+from . import exceptions
 
 
 __version__ = '0.0.1'
