6bd49505bf6ed3a7b312aa9638cdfeb908e6d850
Marek Marecki <marekjm@ozro.pw>
2014-04-14 20:22:04 +0200

    Improved functionality of Messenger util object

--- bearton/util.py
+++ bearton/util.py
@@ -9,27 +9,57 @@ class Messenger:
     """Object used to display messages, i.e. print them to console.
     """
     def __init__(self, verbosity=0, quiet=False, debugging=False, stream=sys.stdout):
-        self._verbosity = verbosity
-        self._quiet = quiet
-        self._debugging = debugging
         self._stream = stream
+        self._verbosity = verbosity
+        self._quiet, self._debugging = quiet, debugging
+        self._line, self._lineend = '', '\n'
+        self._on = {}
 
-    def setVerbosity(self, n):
-        self._vebrosity = n
+    def _send(self, line, keep=False):
+        self._line += line
+        if not keep:
+            self._stream.write(self._line + self._lineend)
+            self._line = ''
 
-    def message(self, msg, verbosity=0):
+    def setlineending(self, ending='\n'):
+        self._lineend = ending
+        return self
+
+    def message(self, msg, verbosity=0, keep=False):
         """Write message to stream.
         """
         if self._quiet: return
         if self._verbosity < verbosity: return
-        msg = '{0}\n'.format(msg)
-        self._stream.write(msg)
+        self._send(msg, keep)
+        return self
 
-    def debug(self, msg):
+    def debug(self, msg, keep=False):
         """Write debug message to stream.
         """
-        msg = 'debug: {0}\n'.format(msg)
-        if self._debugging: self._stream.write(msg)
+        msg = '{0}{1}'.format(('debug: ' if not self._line else ''), msg)
+        if self._debugging: self._send(msg, keep)
+        return self
+
+    def onok(self, msg):
+        self._on['ok'] = msg
+        return self
+
+    def onfail(self, msg):
+        self._on['fail'] = msg
+        return self
+
+    def call(self, callable, *args, **kwargs):
+        try:
+            callable(*args, **kwargs)
+            self.debug(msg=self._on['ok'], keep=True)
+        except Exception as e:
+            self.debug(msg=self._on['fail'].replace('{:error_msg}', str(e)).replace('{:error_type', str(type(e))[8:-2]), keep=True)
+        finally:
+            pass
+
+    def report(self):
+        self._on = {}
+        self.debug('')
 
 
 def readfile(path, encoding='utf-8'):
