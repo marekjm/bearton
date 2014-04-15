8094af6af92c24d2bc6df003fb21d2ca8105a577
Marek Marecki <marekjm@ozro.pw>
2014-04-15 15:07:14 +0200

    Improved init logic

--- bearton/init.py
+++ bearton/init.py
@@ -1,54 +1,59 @@
 """Module responsible for initilaizing Bearton locals.
 """
 
+import json
 import os
 import shutil
 
 from . import util
 
 
-def _newdirs(where, msgr=None):
+def _newdirs(target, msgr=None):
     """Create require directories.
     """
     dirs = [('.bearton',),
             ('.bearton', 'schemes'),
             ('.bearton', 'db'),
+            ('.bearton', 'db', 'pages'),
+            ('.bearton', 'db', 'base'),
             ('assets',),
             ('data',),
             ]
     for parts in dirs:
-        path = os.path.join(where, *parts)
+        path = os.path.join(target, *parts)
         os.mkdir(path)
         if msgr is not None: msgr.message('creating: {0}'.format(path), 2)
 
-def _newconf(where, msgr):
+def _newconf(target, msgr):
     """Create empty config file.
     """
-    config_path = os.path.join(where, '.bearton', 'config.json')
-    util.writefile(config_path, '{}')
-    if msgr is not None: msgr.message('written empty config file to {0}'.format(config_path), 1)
+    config_path = os.path.join(target, '.bearton', 'config.json')
+    config = {'scheme': 'default'}
+    util.writefile(config_path, json.dumps(config))
+    if msgr is not None: msgr.message('written default config file to {0}'.format(config_path), 1)
 
-def _copyschemes(where, schemes_path, msgr):
+def _copyschemes(target, schemes_path, msgr):
     """Copy schemes to new Bearton local site repository.
     """
-    msgr.debug('TODO: implement me!')
-    raise
+    msgr.message('target: {0}'.format(target))
+    msgr.message('schemes: {0}'.format(schemes_path))
+    for scheme in os.listdir(schemes_path): shutil.copytree(os.path.join(schemes_path, scheme), os.path.join(target, '.bearton', 'schemes', scheme))
 
-def new(where, schemes='', msgr=None):
+def new(target, schemes='', msgr=None):
     """Creates a new Bearton local repo.
     """
-    if not os.path.isdir(where): raise NotADirectoryError(where)
-    _newdirs(where, msgr)
-    _newconf(where, msgr)
-    if schemes: _copyschemes(where, schemes, msgr)
+    if not os.path.isdir(target): raise NotADirectoryError(target)
+    _newdirs(target, msgr)
+    _newconf(target, msgr)
+    if schemes: _copyschemes(target, schemes, msgr)
 
-def rm(where, msgr=None):
+def rm(target, msgr):
     for part in ['assets', 'data']:
-        path = os.path.join(where, part)
+        path = os.path.join(target, part)
         if os.path.isdir(path):
             shutil.rmtree(path)
-            if msgr is not None: msgr.message('removed: {0}'.format(path), 2)
-    path = os.path.join(where, '.bearton')
+            msgr.message('removed: {0}'.format(path), 2)
+    path = os.path.join(target, '.bearton')
     if os.path.isdir(path):
         shutil.rmtree(path)
-        if msgr is not None: msgr.message('removed Bearton local from {0}'.format(path), 1)
+        msgr.message('removed Bearton local from {0}'.format(path), 1)
--- ui/bearton-init.py
+++ ui/bearton-init.py
@@ -19,7 +19,7 @@ ui.parse()
 
 msgr = bearton.util.Messenger(verbosity=0, debugging=('--debug' in ui))
 
-SCHEMES_PATH = '/usr/share/bearton/schemes'
+SCHEMES_PATH = (ui.get('-s') if '--schemes' in ui else '/usr/share/bearton/schemes')
 
 if str(ui) == 'init':
     if '--where' in ui:
@@ -31,6 +31,14 @@ if str(ui) == 'init':
         schemes_path = ''
     else:
         schemes_path = (ui.get('-s') if '--schemes' in ui else SCHEMES_PATH)
-    if '--force' in ui: bearton.init.rm(path)
-    bearton.init.new(where=path, schemes=schemes_path, msgr=msgr)
+    if '--force' in ui: bearton.init.rm(path, msgr)
+    bearton.init.new(target=path, schemes=schemes_path, msgr=msgr)
     msgr.message('initialized Bearton local in {0}'.format(path), 0)
+elif str(ui) == 'rm':
+    target = (ui.get('-t') if '--target' in ui else '.')
+    target = os.path.abspath(target)
+    if '.bearton' in os.listdir(target):
+        bearton.init.rm(target, msgr)
+        msgr.debug('removed Bearton repository from {0}'.format(target))
+    else:
+        msgr.debug('no Bearton repository found in {0}'.format(target))
