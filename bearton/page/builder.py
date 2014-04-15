efd6f1051847fab02e6f0e1beb8a8d095ab923a4
Marek Marecki <marekjm@ozro.pw>
2014-04-15 14:43:06 +0200

    Build function can render final pages

--- bearton/page/builder.py
+++ bearton/page/builder.py
@@ -10,60 +10,6 @@ from .. import config
 from .. import db
 
 
-def getcontext(schemepath, element):
-    elpath = os.path.join(schemepath, 'elements', element)
-    cntxt = util.readfile(os.path.join(elpath, 'context.json'))
-    try:
-        context, error = json.loads(cntxt), None
-    except Exception as e:
-        context, error = {}, e
-    finally:
-        pass
-    if error is not None:
-        raise type(e)('while loading context for: {0}: {1}'.format(os.path.abspath(elpath), e))
-    return context
-
-def getmeta(schemepath, element):
-    elpath = os.path.join(schemepath, 'elements', element)
-    meta = util.readfile(os.path.join(elpath, 'meta.json'))
-    try:
-        meta, error = json.loads(meta), None
-    except Exception as e:
-        meta, error = {}, e
-    finally:
-        pass
-    if error is not None:
-        raise type(e)('while loading metadata for: {0}: {1}'.format(os.path.abspath(elpath), e))
-    return meta
-
-
-class Element:
-    """Class used to build objects.
-    """
-    def __init__(self, scheme, element):
-        self._scheme = scheme
-        self._element = element
-        self._template, self._context = '', {}
-        self._meta = {}
-
-    def _gathercontexts(self, schemepath):
-        contexts = self._meta['requires']['contexts'][:]
-        for element in self._meta['requires']['contexts']:
-            meta = getmeta(schemepath, element)
-            contexts.extend([req for req in meta['requires']['contexts'] if req not in contexts])
-        return contexts
-
-    def _loadcontext(self, schemepath):
-        required = self._gathercontexts(schemepath)
-        required.insert(0, self._element)
-        for element in required:
-            cntxt = getcontext(schemepath, element)
-            for k, v in cntxt.items():
-                if k in self._context:
-                    warnings.warn('bearton: warning: conflicting key in required contexts: {0}: {1} -> {2}'.format(k, self._context[k], v))
-                self._context[k] = v
-
-
 def gathercontexts(schemes, scheme, contexts, msgr):
     msgr.debug('required contexts: {0}'.format(contexts))
     gathered = []
@@ -98,3 +44,9 @@ def build(path, schemes, page, msgr):
     msgr.debug('context: {0}'.format(context))
     context = loadcontexts(schemes, meta['scheme'], context, gathered)
     msgr.debug('final context: {0}'.format(context))
+    msgr.debug('reading template')
+    template = util.readfile(os.path.join(schemes, meta['scheme'], 'elements', meta['name'], 'template.mustache'))
+    rendered = ''
+    rendered = muspyche.make(template, context, lookup=[os.path.join(schemes, meta['scheme'], 'elements')])
+    msgr.message('\n')
+    msgr.message(rendered, 0)
