9846f37b2a30be2b947b4dc38cb7a2405cfd0d07
Marek Marecki <marekjm@ozro.pw>
2014-04-17 23:50:33 +0200

    Configuration can now be protected with it's quasi-singleton quard-lock (multiple runtime copies can exist which may be modified, but when one is loaded with guard all become read-only - this is a safety measure)

--- bearton/config.py
+++ bearton/config.py
@@ -8,12 +8,13 @@ from .exceptions import *
 class Configuration:
     """Class implementing interface to Bearton site configuration.
     """
+    # class variable, so when one copy of configuratin is loaded as guarded all become
+    # the instance that set config to guarded stance becomes a guard and only it can unguard the config (by calling .unload())
+    _guarded, _guard = False, None
+
     def __init__(self, path='.'):
         self._path = path
         self._conf = {}
-
-    def __str__(self):
-        return str(self._conf)
 
     def __contains__(self, key):
         return key in self._conf
@@ -40,18 +41,35 @@ class Configuration:
         else:
             return path
 
-    def load(self):
+    def load(self, guard=False):
         """Loads configuration file.
+        If `guard` is true, it will guard the file from being written; the runtime copy may be modified at will however.
         """
+        if Configuration._guarded is not True:
+            Configuration._guarded = guard
+            Configuration._guard = self
         self._path = self._getpath()
-        self._conf = json.loads(util.readfile(self._path))
+        conf = json.loads(util.readfile(self._path))
+        if conf is not None: self._conf = conf
+        else: self.default()
         return self
+
+    def unload(self):
+        """Unloads configuration from the instance.
+        """
+        if Configuration._guard == self and Configuration._guarded is True: Configuration._guarded = False
+        self._conf = None
+
+    def default(self):
+        """Return configuration to default state.
+        """
+        self._conf = {'scheme': 'default'}
 
     def store(self):
         """Stores configration file and
         writes any changes made.
         """
-        util.writefile(path=self._path, s=json.dumps(self._conf))
+        if Configuration._guarded is not True: util.writefile(path=self._path, s=json.dumps(self._conf))
         return self
 
     def get(self, key):
