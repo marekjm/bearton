3698efb2e4f10536f2949f7fabbb44be96ed69e1
Marek Marecki <marekjm@ozro.pw>
2014-04-18 22:54:59 +0200

    Messenger is now required by more functions

--- bearton/page/page.py
+++ bearton/page/page.py
@@ -8,10 +8,10 @@ from .. import config
 from .. import db
 
 
-def new(path, schemes_path, scheme, element, msgr=None):
+def new(path, schemes_path, scheme, element, msgr):
     hashed = hashlib.sha256(base64.b64encode(os.urandom(64))).hexdigest()
-    if msgr is not None: msgr.debug(hashed)
-    if msgr is not None: msgr.debug('{0} {1}'.format(path, os.path.isdir(path)))
+    msgr.debug(hashed)
+    msgr.debug('{0} {1}'.format(path, os.path.isdir(path)))
     hashpath = os.path.join(path, '.bearton', 'db', 'pages', hashed)
     msgr.debug('creating databse entry: {0}'.format(hashpath))
     os.mkdir(hashpath)
@@ -24,6 +24,7 @@ def new(path, schemes_path, scheme, element, msgr=None):
         msgr.call(shutil.copy, os.path.join(element_path, f), os.path.join(hashpath, f))
         msgr.report()
     entry = db.Entry(*os.path.split(hashpath)).load()
+    msgr.debug('scheme used: {0}'.format(scheme))
     if 'scheme' not in entry._meta: entry.setinmeta('scheme', scheme)
     if 'name' not in entry._meta: entry.setinmeta('name', element)
     entry.store()
@@ -42,7 +43,7 @@ def newbase(path, schemes_path, scheme, element, msgr=None):
         msgr.call(shutil.copy, os.path.join(element_path, f), os.path.join(basepath, f))
         msgr.report()
 
-def edit(path, page, msgr=None):
+def edit(path, page, msgr):
     msgr.debug('editing in: {0}'.format(path))
     path = os.path.join(path, '.bearton', 'db')
     msgr.debug('path to database: {0} ({1})'.format(path, ('OK' if os.path.isdir(path) else 'FAIL')))
