60a46dbaf5d925844801e2e84331b599a33fa34f
Marek Marecki <marekjm@ozro.pw>
2014-05-18 15:16:42 +0200

    Changes to config UI

--- ui/bearton-config.py
+++ ui/bearton-config.py
@@ -32,10 +32,13 @@ SCHEMES_PATH = (ui.get('-S') if '--schemes' in ui else bearton.util.getschemespa
 # Creating widely used objects
 msgr = bearton.util.Messenger(verbosity=int('--verbose' in ui), debugging=('--debug' in ui), quiet=('--quiet' in ui))
 db = bearton.db.db(path=SITE_PATH).load()
-config = bearton.config.Configuration(path=SITE_PATH).load()
+config = bearton.config.Configuration(path=SITE_PATH).load(guard=True)
 
 
 if bearton.util.inrepo(path=TARGET) and str(ui) == 'get':
+    if len(ui.arguments) > 1:
+        msgr.message('fail: invalid number of operands: expected at most 1 but got {0}'.format(len(ui.arguments)))
+        exit(1)
     if '--list' in ui:
         if '--verbose' in ui:
             output = dict(config.kvalues())
@@ -60,26 +63,29 @@ if bearton.util.inrepo(path=TARGET) and str(ui) == 'get':
         if '--json' in ui: output = json.dumps(output)
         msgr.message(output, 0)
 elif bearton.util.inrepo(path=TARGET) and str(ui) == 'set':
-    key, value = '', ''
-    if ui.arguments: key = ui.arguments.pop(0)
-    if ui.arguments: value = ui.arguments.pop(0)
-    if '--key' in ui: key = ui.get('-k')
-    if '--value' in ui: value = ui.get('-v')
-    msgr.debug('{0} -> {1}'.format(key, value))
-    config.set(key, value)
+    if len(ui.arguments) > 2:
+        msgr.message('fail: invalid number of operands: expected at most 2 but got {0}'.format(len(ui.arguments)))
+    else:
+        key, value = '', ''
+        if ui.arguments: key = ui.arguments.pop(0)
+        if ui.arguments: value = ui.arguments.pop(0)
+        if '--key' in ui: key = ui.get('-k')
+        if '--value' in ui: value = ui.get('-v')
+        msgr.debug('{0} -> {1}'.format(key, value))
+        if key: config.unguard().set(key, value).guard()
 elif bearton.util.inrepo(path=TARGET) and str(ui) == 'rm':
-    keys = []
-    if ui.arguments: keys = [k for k in ui.arguments]
+    keys = [k for k in ui.arguments]
     if '--key' in ui: keys = [ui.get('-k')]
+    config.unguard()
     if '--pop' in ui:
         msgr.message(config.pop(keys[0]), 0)
     else:
         for k in keys: config.remove(k)
+    config.guard()
 elif str(ui) == '':
     if '--version' in ui: msgr.message(('bearton version {0}' if '--verbose' in ui else '{0}').format(bearton.__version__), 0)
     if '--help' in ui:
-        print('\n'.join(clap.helper.Helper(ui).help()))
-
+        msgr.message('\n'.join(clap.helper.Helper(ui).help()))
 
 
 # Storing widely used objects state
