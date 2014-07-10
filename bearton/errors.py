7dc9b26e25c7ce1f06026dd5726b7f884eb9adbd
Marek Marecki <marekjm@ozro.pw>
2014-07-10 16:25:49 +0200

    Added errors module

---
+++ bearton/errors.py
@@ -0,0 +1,17 @@
+"""File defining Bearto-specific exceptions.
+"""
+
+class BeartonError(Exception):
+    """Base class for all Bearton-specific exceptions.
+    """
+    pass
+
+
+class BeartonEnvironmentError(BeartonError):
+    """Raised when something related to Bearton-specific environment goes wrong.
+    """
+    pass
+
+
+class RepositoryNotFoundError(BeartonEnvironmentError):
+    pass
