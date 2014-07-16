7f8e5ac96d25f3a6b4c1f8bc3b51b09778d3a847
Marek Marecki <marekjm@ozro.pw>
2014-07-17 00:09:49 +0200

    Refactored code in scheme inspector, added new function: getSchemePath()

--- bearton/schemes/inspector.py
+++ bearton/schemes/inspector.py
@@ -7,30 +7,44 @@ import os
 from .. import util
 
 
-def lselements(path, scheme):
+def lselements(scheme):
     """Returns a list of elements of given scheme.
     Parameters:
 
     - path:     path to the directory scheme is located in,
     - scheme:   name of the scheme,
     """
-    return os.listdir(os.path.join(path, scheme, 'elements'))
+    return os.listdir(os.path.join(scheme, 'elements'))
+
 
 def getElementMetas(scheme):
     """Return list of two-tuples: (name, meta).
     """
-    path = os.path.join(scheme, 'elements')
-    elements = lselements(scheme)
+    path, scheme = os.path.split(scheme)
+    elements = lselements(path, scheme)
     metas = []
     for i in elements:
-        meta = json.loads(util.io.read(os.path.join(path, i, 'meta.json')))
+        meta = json.loads(util.io.read(os.path.join(path, scheme, 'elements', i, 'meta.json')))
         metas.append( (i, meta) )
     return metas
 
-def getElementMeta(path, scheme, element):
+
+def getElementMeta(scheme, element):
     """Returns meta of element in given scheme.
     """
-    return json.loads(util.io.read(os.path.join(path, scheme, 'elements', element, 'meta.json')))
+    return json.loads(util.io.read(os.path.join(scheme, 'elements', element, 'meta.json')))
+
+
+def getSchemePath(scheme, target=None):
+    """Returns path to the scheme.
+    The `target` parameter point to Bearton repository in which to begin searching.
+    If it is None, use util.env functions to find a suitable repository.
+    """
+    schemes = util.env.listschemes(util.env.getschemespaths(target if target is not None else util.env.getrepopath()))
+    candidates = [path for schm, path in schemes if schm == scheme]
+    if not candidates: raise FileNotFoundError('could not find a path for scheme: {0}'.format(scheme))
+    return os.path.join(candidates[0], scheme)
+
 
 def getSchemeMeta(scheme):
     """Return meta of whole scheme.
