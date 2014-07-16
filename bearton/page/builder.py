f906ddd84d8ef6da12e6d2f65f42a604932c8749
Marek Marecki <marekjm@ozro.pw>
2014-07-17 00:13:08 +0200

    Reworked page/builder module, along with various fixes and improvements to documentation

--- bearton/page/builder.py
+++ bearton/page/builder.py
@@ -23,52 +23,74 @@ from .. import db
 from .. import schemes
 
 
-def gathercontexts(path, pagepath, msgr, what='base'):
-    pagemeta = json.loads(util.readfile(os.path.join(pagepath, 'meta.json')))
-    msgr.debug('directly required contexts ({1}): {0}'.format(pagemeta['requires'][what], what))
-    gathered = pagemeta['requires'][what][:]
+def gathercontexts(path, pagepath, pagemeta, what='base', messenger=None):
+    """Gather all contexts required by the page.
+    This function will resolve context dependencies.
+    """
+    gathered = (pagemeta['requires'][what][:] if what in pagemeta['requires'] else [])
+    if messenger is not None:
+        messenger.debug('directly required contexts ({0}) for element "{1}": {2}'.format(what, pagemeta['name'], ', '.join(gathered)))
+        messenger.indent()
     for c in gathered:
-        if c[0] == '#':
+        if c[0] in ['#', '?']:
             required = [c]
         else:
-            required = schemes.inspector.getMeta(pagemeta['scheme'], c)['requires'][what]
+            cmeta = schemes.inspector.getElementMeta(schemes.inspector.getSchemePath(pagemeta['scheme'], path), c)
+            has = (what in cmeta['requires'])
+            required = (cmeta['requires'][what] if has else [])
+            if messenger is not None and required: messenger.debug('dependency contexts required by element "{0}": {1}'.format(c, ', '.join(required)))
         gathered.extend([el for el in required if el not in gathered])
-    msgr.debug('resolved required contexts ({1}): {0}'.format(gathered, what))
+    if messenger is not None:
+        messenger.dedent()
+        messenger.debug('resolved required contexts ({0}) for element "{1}": {2}'.format(what, pagemeta['name'], ', '.join(gathered)))
     return gathered
 
-def loadcontexts(path, schemes, scheme, context, required, msgr):
-    for element in required:
-        if element[0] == '#':
-            key, query = tuple(element[1:].split(':', 1))
-            msgr.debug('creating context with key "{0}" from query: {1}'.format(key, query))
-            got = db.db(path=path).load().rawquery(query=query)
-            cntxt = [entry._context for key, entry in got]
-            for i, en in enumerate(got):
-                en = en[1]
-                cntxt[i]['meta'] = en._meta
-            cntxt = {key: cntxt}
-        else:
-            cntxt = json.loads(util.readfile(os.path.join(schemes, scheme, 'elements', element, 'context.json')))
-        for k, v in cntxt.items():
-            if k in context: msgr.message('warning: key conflict in context: {0}: {1} -> {2}'.format(k, context[k], v))
-            context[k] = v
+
+def _mergecontexts(context, update, messenger=None):
+    """Merges two contexts together.
+    """
+    for k, v in update.items():
+        if k in context and messenger is not None: messenger.message('warning: key conflict in context, leaving: {0}: {1} -> {2}'.format(k, context[k], v))
+        context[k] = v
     return context
 
-def loadbasecontexts(path, schemes, scheme, context, required, msgr):
+
+def loadcontexts(path, scheme, context, required, messenger=None):
+    """Loads required additional contexts for requested page.
+    """
+    # iterate over required elements, they tell us what additional contexts must be loaded
     for element in required:
-        if element[0] == '#':
-            continue
-        conpath = os.path.join(path, '.bearton', 'db', 'base', element, 'context.json')
-        if not os.path.isfile(conpath):
-            msgr.debug('warning: loading default context for base element: {0}'.format(element))
-            conpath = os.path.join(schemes, scheme, 'elements', element, 'context.json')
-        cntxt = json.loads(util.readfile(conpath))
-        for k, v in cntxt.items():
-            if k in context: msgr.message('warning: key conflict in context: {0}: {1} -> {2}'.format(k, context[k], v))
-            context[k] = v
+        if element[0] in ['#', '?']:    # if element begins with # or ? it is a query to be run to get context out od the database
+            key, query = tuple(element[1:].split(':', 1)) # set result of the query to the given key
+            if messenger is not None: messenger.debug('creating context with key "{0}" from query: {1}'.format(key, query))
+            got = db.db(path=path).load().rawquery(query=query)
+            # extract contexts out of the query result
+            cntxt = [entry._context for key, entry in got]
+            # add metadata of queried-out elements to the context
+            for i, en in enumerate(got): cntxt[i]['meta'] = en[1]._meta
+            cntxt = {key: cntxt}
+        else:                           # otherwise, just load the default context for this element (from scheme)
+            cntxt = json.loads(util.readfile(os.path.join(scheme, 'elements', element, 'context.json')))
+        context = _mergecontexts(context, cntxt, messenger)
     return context
+
+
+def loadbasecontexts(target, scheme, context, required, messenger):
+    """Loads required additional contexts for requested page.
+    """
+    for element in required:
+        if element[0] in ['#', '?']: raise TypeError('queries cannot be used to load base contexts: {0}'.format(element))
+        path = os.path.join(target, 'db', 'base', element, 'context.json')
+        if not os.path.isfile(path):
+            if messenger is not None: messenger.debug('warning: loading default context for base element: {0}'.format(element))
+            path = os.path.join(scheme, 'elements', element, 'context.json')
+        cntxt = json.loads(util.io.read(path))
+        context = _mergecontexts(context, cntxt, messenger)
+    return context
+
 
 def _getstuff(path, schemes, page, msgr):
+    raise Exception('FIXME,DOCME')
     path = os.path.abspath(path)
     dbpath = os.path.join(path, '.bearton', 'db', 'pages')
     pagepath = os.path.join(dbpath, page)
@@ -77,26 +99,66 @@ def _getstuff(path, schemes, page, msgr):
     msgr.debug('pagepath: {0}'.format(pagepath))
     meta = json.loads(util.readfile(os.path.join(pagepath, 'meta.json')))
     context = json.loads(util.readfile(os.path.join(pagepath, 'context.json')))
-    context = loadcontexts(path, schemes, meta['scheme'], context, gathercontexts(path, pagepath, msgr, 'contexts'), msgr)
-    context = loadbasecontexts(path, schemes, meta['scheme'], context, gathercontexts(path, pagepath, msgr, 'base'), msgr)
     template = os.path.join(schemes, meta['scheme'], 'elements', meta['name'], 'template.mustache')
     msgr.debug('reading template: {0}'.format(template))
     template = util.readfile(template)
     return (meta, context, template)
 
-def render(path, schemes, page, msgr):
-    meta, context, template = _getstuff(path, schemes, page, msgr)
-    lookup = [os.path.join(schemes, meta['scheme'], 'elements'), os.path.join(path, '.bearton', 'db', 'pages', page)]
-    parsed = muspyche.parser.parse(template, lookup)
-    context = muspyche.context.ContextStack(context)
-    renderer = muspyche.renderer.Renderer(parsed, context, lookup)
-    if markdown is not None:
+
+def _getmeta(pagepath, messenger=None):
+    """Returns metadata of the requested page.
+    """
+    meta = json.loads(util.io.read(os.path.join(pagepath, 'meta.json')))
+    return meta
+
+
+def _getcontext(path, pagepath, meta, messenger=None):
+    """Returns context of the requested page.
+    """
+    context = json.loads(util.io.read(os.path.join(pagepath, 'context.json')))
+    scheme_name = (meta['scheme'] if 'scheme' in meta else 'default')
+    if messenger is not None: messenger.debug('defined scheme used: {0}'.format(scheme_name))
+    scheme_path = [path for schm, path in util.env.listschemes(util.env.getschemespaths()) if schm == scheme_name][0]
+    if messenger is not None: messenger.debug('defined scheme path: {0}'.format(scheme_path))
+    scheme = os.path.join(scheme_path, scheme_name)
+    context = loadcontexts(path, scheme, context, gathercontexts(path, pagepath, meta, 'contexts', messenger), messenger)
+    context = loadbasecontexts(path, scheme, context, gathercontexts(path, pagepath, meta, 'base', messenger), messenger)
+    return context
+
+
+def render(target, page, messenger):
+    """Renders selected page and returns it as string.
+    Parameters:
+
+    *   target: path to the repository,
+    *   use:    three-tuple, (
+    """
+    #meta, context, template = _getstuff(path, schemes, page, msgr)
+    #lookup = [os.path.join(schemes, meta['scheme'], 'elements'), os.path.join(path, '.bearton', 'db', 'pages', page)]
+    #parsed = muspyche.parser.parse(template, lookup)
+    #context = muspyche.context.ContextStack(context)
+    #renderer = muspyche.renderer.Renderer(parsed, context, lookup)
+    #if markdown is not None:
         # apply Markdown post-renderer for out-of-the-box Markdown support for
         # Markdown partials (e.g. articles written in Markdown)
-        renderer.addpost('^markdown/', markdown.markdown)
-    return renderer.render()
+    #    renderer.addpost('^markdown/', markdown.markdown)
+    #return renderer.render()
+    pagepath = os.path.join(target, 'db', 'pages', page)
+    if messenger is not None: messenger.debug('rendering: set pagepath: {0}'.format(pagepath))
+    meta = _getmeta(pagepath, messenger)
+    if messenger is not None: messenger.debug('rendering: got metadata')
+    context = _getcontext(path=target, pagepath=pagepath, meta=meta, messenger=messenger)
+    if messenger is not None: messenger.debug('rendering: got context')
+    rendered = ''
+    return rendered
 
 def build(path, schemes, page, msgr):
+    """Parameters:
+    *   target: target directory of the build not repo,
+    *   path:   path to scheme directory,
+    *   page:   page id to render,
+    """
+    raise Exception('FIXME,DOCME')
     meta = json.loads(util.readfile(os.path.join(path, '.bearton', 'db', 'pages', page, 'meta.json')))
     output = os.path.join(path, util.expandoutput(meta['output']))
     msgr.debug('written file to: {0}'.format(output))
